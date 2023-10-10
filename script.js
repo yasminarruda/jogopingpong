38cb7536422199259a3cbb0e439cd97a43ad81c3
yasminarruda <108303633+yasminarruda@users.noreply.github.com>
2023-10-10 11:28:05 -0300

    pinguepongue

--- script.js
+++ script.js
@@ -27,10 +27,10 @@ var campoX=0//Posição 'X' do campo.
 var campoY=0//Posição 'Y' do campo.
 var campoW=960//Largura do campo.
 var campoH=500//Altura do campo.
-var barraW=20//Largura da barra.
-var barraH=140//Altura da barra.
-var bolaW=20//Largura da bola.
-var bolaH=20//Alltura da bola.
+var barraW=28//Largura da barra.
+var barraH=150//Altura da barra.
+var bolaW=25//Largura da bola.
+var bolaH=25//Alltura da bola.
 
 //Direção
 var bolaX//DIreção da bola em 'X'.
